8df1a5c632e991de10897b860fd957e1474aa24f
crazymanner <chojunho427@gmail.com>
2017-12-30 22:56:37 +0900

    v 1.1.0
    info +

--- phone.js
+++ phone.js
@@ -5,3 +5,22 @@ $(document).ready(function() {
        $('header').toggleClass( "shownav" );
     }); 
  });
+
+ $('.toggle').on('click', function(event) {
+    if (!$(this).hasClass("open")) {
+      $(this).siblings(".content").slideDown();
+      $(this).addClass("open");   
+    }else{
+       $(this).siblings(".content").slideUp();
+       $(this).removeClass("open"); 
+       $(this).blur();
+    }
+  });
+  
+  $(".bottom .toggle").click(function(){
+  
+    if($('.bottom .content').is(":visible")){
+        $("html, body").animate({scrollTop:$(this).offset().top-10});
+    }
+    
+  });
